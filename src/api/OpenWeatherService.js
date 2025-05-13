9d986ab48ffd7cefbd5afabb5b6664f526819c24
Harjit Kaur <49105329+harkaur02@users.noreply.github.com>
2025-05-13 11:54:11 -0400

    Update OpenWeatherService.js

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = '2899a5e35b570dc77f1fab92fb48d7c2';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
